cb8fb044c7699161d498a1db3e93f5549e5d0ebe
Erum Sikander <mrsikanderghouri@gmail.com>
2024-06-09 12:22:44 +0500

    syntex error complete

---
+++ new.js
@@ -0,0 +1,6 @@
+"use strict";
+// Sir-Zia-repo-02-practice:
+// Syntax_error define:
+Object.defineProperty(exports, "__esModule", { value: true });
+let message = "Erum Sikander";
+console.log(message);
---
+++ new.ts
@@ -0,0 +1,11 @@
+
+// Sir-Zia-repo-02-practice:
+// Syntax_error define:
+
+// lett message:string  = "Erum Sikander";
+// console.log(message);
+
+let message:string  = "Erum Sikander";
+console.log(message);
+
+
